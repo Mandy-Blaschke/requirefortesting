0a13a9e022137c34c62559f56fca6732bea952de
Mandy Blaschke <post@mandy-blaschke.de>
2023-12-11 19:04:06 +0100

    wip login function

--- src/main/java/de/mandyblaschke/requirefortesting/LoginBean.java
+++ src/main/java/de/mandyblaschke/requirefortesting/LoginBean.java
@@ -2,18 +2,75 @@ package de.mandyblaschke.requirefortesting;
 
 import jakarta.enterprise.context.SessionScoped;
 import jakarta.inject.Named;
+
 import java.io.Serializable;
 
 @Named
 @SessionScoped
 public class LoginBean implements Serializable {
-    private String username;
 
-    public String getUsername() {
-        return username;
+    // Username + Getter + Setter
+    private String user = "";
+
+    public String getUser() {
+        return user;
     }
 
-    public void setUsername(String username) {
-        this.username = username;
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    // Error + Getter + Setter
+    private String errorMessage = "";
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    // isAuthorized + Getter + Setter
+    private boolean isAuthorized = false;
+
+    public boolean isAuthorized() {
+        return isAuthorized;
+    }
+
+    public void setAuthorized(boolean authorized) {
+        isAuthorized = authorized;
+    }
+
+    // Rolle + Getter + Setter
+
+    private String role;
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    // Login Funktion
+    public String login() {
+        boolean loginSuccess = false;
+
+        if (user.equals("yolo")) {
+            setRole("re");
+            loginSuccess = true;
+        }
+
+        if (loginSuccess) {
+            errorMessage = "";
+            if (role.equals("re")) {
+                return "re.xhtml?faces-redirect=true";
+            }
+        } else {
+            errorMessage = "User nicht gefunden.";
+        }
+        return null;
     }
 }
